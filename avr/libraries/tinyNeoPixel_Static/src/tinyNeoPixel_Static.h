633aa4545dcb88cc29ea55845a742b7665067268
MCUdude <hansibull@gmail.com>
2026-02-16 19:38:42 +0100

    Update Neopixel libraries from ATTinyCore 2.0.0

--- avr/libraries/tinyNeoPixel_Static/src/tinyNeoPixel_Static.h
+++ avr/libraries/tinyNeoPixel_Static/src/tinyNeoPixel_Static.h
@@ -1,6 +1,16 @@
 /*--------------------------------------------------------------------
   This file is part of the tinyNeoPixel library, derived from
-  Adafruit_NeoPixel.
+  Adafruit_NeoPixel. Modification was carried out between 2016 and
+  2021 by Spence Konde for ATTinyCore, and later megaTinyCore and
+  DxCore. The latest modification eliminates the menu for the port -
+  It uses ST instructions now, and while the assembly is uglier, the
+  result is often smaller code that doesn't need a stupid menu option.
+
+  This is the "static allocation" version of this library. You must
+  pass a pointer to a suitable array to use as the frame buffer, and
+  you must ensure that the pin being used is set output. We don't do
+  that here, so that you can use direct port writes to set it output
+  while using less flash.
 
   NeoPixel is free software: you can redistribute it and/or modify
   it under the terms of the GNU Lesser General Public License as
@@ -95,9 +105,10 @@
 
 /* A PROGMEM (flash mem) table containing 8-bit unsigned sine wave (0-255).
    Copy & paste this snippet into a Python REPL to regenerate:
-import math
-for x in range(256):
-    print("{:3},".format(int((math.sin(x/128.0*math.pi)+1.0)*127.5+0.5))),
+
+  import math
+  for x in range(256):
+    print("{:3},".format(int((math.sin(x/128.0*math.pi)+1.0)*127.5 + 0.5))),
     if x&15 == 15: print
 */
 static const uint8_t PROGMEM _NeoPixelSineTable[256] = {
@@ -120,10 +131,9 @@ static const uint8_t PROGMEM _NeoPixelSineTable[256] = {
 
 /* Similar to above, but for an 8-bit gamma-correction table.
    Copy & paste this snippet into a Python REPL to regenerate:
-import math
-gamma=2.6
-for x in range(256):
-    print("{:3},".format(int(math.pow((x)/255.0,gamma)*255.0+0.5))),
+  import math
+  for x in range(256):
+    print("{:3},".format(int((math.sin(x/128.0*math.pi)+1.0)*127.5 + 0.5))),
     if x&15 == 15: print
 */
 static const uint8_t PROGMEM _NeoPixelGammaTable[256] = {
@@ -162,6 +172,7 @@ class tinyNeoPixel {
     setPixelColor(uint16_t n, uint8_t r, uint8_t g, uint8_t b),
     setPixelColor(uint16_t n, uint8_t r, uint8_t g, uint8_t b, uint8_t w),
     setPixelColor(uint16_t n, uint32_t c),
+    fill(uint32_t c = 0, uint16_t first = 0, uint16_t count = 0),
     setBrightness(uint8_t),
     clear();
   uint8_t
@@ -231,7 +242,7 @@ class tinyNeoPixel {
   static uint32_t   Color(uint8_t r, uint8_t g, uint8_t b, uint8_t w) {
     return ((uint32_t)w << 24) | ((uint32_t)r << 16) | ((uint32_t)g <<  8) | b;
   }
-  static uint32_t   ColorHSV(uint16_t hue, uint8_t sat=255, uint8_t val=255);
+  static uint32_t   ColorHSV(uint16_t hue, uint8_t sat = 255, uint8_t val = 255);
   /*!
     @brief   A gamma-correction function for 32-bit packed RGB or WRGB
              colors. Makes color transitions appear more perceptially
